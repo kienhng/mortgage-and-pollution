ed2e4f852fa2297e395ea7677cdb6cbf8f796950
kienhng <38973110+kienhng@users.noreply.github.com>
2023-07-11 23:58:47 +0100

    add panel generating code

--- Code/clean-HMDA.R
+++ Code/clean-HMDA.R
@@ -4,7 +4,7 @@ need.install <- packages[!(packages %in% installed.packages()[,"Package"])]
 lapply(need.install, install.packages, character.only=T)
 lapply(packages, library, character.only=T)
 
-## Old code to save and load first data file
+# Old code to save and load first data file
 # hmda_raw <- as.data.table(read_dta(paste0(wd,hmda.folder,"hmda_2018-2021.dta")))
 # hmda2018=hmda_raw[year==2018]
 # hmda2019=hmda_raw[year==2019]
@@ -21,9 +21,7 @@ load(paste0(wd,hmda.folder,"hmda2020.RData"))
 load(paste0(wd,hmda.folder,"hmda2021.RData"))
 lapply(list(hmda2018,hmda2019,hmda2020,hmda2021), as.data.table)
 
-#---- Compare HMDA and TRI data on ID ----
-##---- Clean and Merge ----
-# Clean HMDA data
+#---- Subset HMDA data ----
 raw_list <- list(hmda2018,hmda2019,hmda2020,hmda2021)
 new_list <- list(1:4)
 
@@ -38,29 +36,14 @@ for (i in 1:4) {
                                              ]
 }
 
-hmda2018_cl <- new_list[[1]]
-hmda2019_cl <- new_list[[2]]
-hmda2020_cl <- new_list[[3]]
-hmda2021_cl <- new_list[[4]]
+hmda2018_samp <- new_list[[1]]
+hmda2019_samp <- new_list[[2]]
+hmda2020_samp <- new_list[[3]]
+hmda2021_samp <- new_list[[4]]
 
-# Collect all unique tracts in HMDA 2018
-# hmda18_tracts <- test2018[,.N,census_tract][,census_tract]
-# hmda18_county <- test2018[,.N,st_cnty_fips][,st_cnty_fips]
+## Create subset of HMDA
+hmda_samp <- rbind(hmda2018_samp,hmda2019_samp,hmda2020_samp,hmda2021_samp)
+hmda_samp[,year_fips := paste0(year,"-",st_cnty_fips)]
 
-# Merge data test
-# setkey(test2018,fips)
-# setkey(tri_match, fips)
-
-## Summarise HMDA data by year-county
-hmda_cl <- rbind(hmda2018_cl,hmda2019_cl,hmda2020_cl,hmda2021_cl)
-hmda_cl[,year_fips := paste0(year,"-",st_cnty_fips)]
-
-## Summarise TRI data by year-county
-tri_test_summary <- tri_test[,lapply(.SD,mean), by = year_fips]
-tri_test_summary <- tri_state[tri_test_summary, on = "year_fips"]
-tri_test_summary[,c("i.fips","i.year") := NULL]
-
-## Merge TRI and HMDA by year-county
-panel_test <- hmda_test_summary[tri_test_summary, on= "year_fips"]
-panel_test
-write_dta(panel_test,path = paste0(wd,panel.folder,"panel_test.dta"))
+# Export HMDA sample
+saveRDS(hmda_samp,file = paste0(wd,panel.folder,"hmda_samp.rds"))
