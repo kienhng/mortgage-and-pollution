76f9e6fd9c78dd8a6780c8056c70c0da3cbb7e90
kienhng <38973110+kienhng@users.noreply.github.com>
2023-07-30 17:11:33 +0100

    up

--- Code/clean-HMDA.R
+++ Code/clean-HMDA.R
@@ -34,38 +34,48 @@ hmda2019[,over_conflimit := ifelse((loan_amount-CONF.LOAN.LIMIT.2019)>0,2,1)]
 hmda2020[,over_conflimit := ifelse((loan_amount-CONF.LOAN.LIMIT.2020)>0,2,1)]
 hmda2021[,over_conflimit := ifelse((loan_amount-CONF.LOAN.LIMIT.2021)>0,2,1)]
 
+## Create the variable for HMDA states
+hmda2018[,st:=substr(st_cnty_fips,1,nchar(st_cnty_fips)-3)]
+hmda2019[,st:=substr(st_cnty_fips,1,nchar(st_cnty_fips)-3)]
+hmda2020[,st:=substr(st_cnty_fips,1,nchar(st_cnty_fips)-3)]
+hmda2021[,st:=substr(st_cnty_fips,1,nchar(st_cnty_fips)-3)]
+
 raw_list <- list(hmda2018,hmda2019,hmda2020,hmda2021)
 new_list <- list(1:4)
 
 for (i in 1:4) {
-  new_list[[i]] <- raw_list[[i]][loan_term == 360
+  new_list[[i]] <- raw_list[[i]][!(st%in%c("60","66","69","72","78")) # Remove mainland-US territories
+                                 ][loan_term == 360
                                  ][,loan_to_value_ratio := loan_amount/property_value
-                                 ][loan_to_value_ratio > 0 & loan_to_value_ratio < 100.001
-                                   ][rate_spread > -4 & rate_spread < 4
-                                     ][interest_rate < 15
-                                       ][income > 0 & income < quantile(income, probs = 0.999, na.rm = T)
-                                           ][property_value < quantile(property_value, probs = 0.999, na.rm = T)
+                                   ][loan_to_value_ratio > 0 & loan_to_value_ratio < 100 | loan_to_value_ratio == 100
+                                     ][income > 0 & income < quantile(income, probs = 0.99, na.rm = T)
+                                       ][property_value < quantile(property_value, probs = 0.99, na.rm = T)
+                                         & property_value > quantile(property_value, probs = 0.01, na.rm = T)
+                                         ][rate_spread < quantile(rate_spread, probs = 0.99, na.rm = T)
+                                           & rate_spread > quantile(rate_spread, probs = 0.01, na.rm = T)
+                                           ][interest_rate < 15
                                              # New variable loan_to_income
-                                             ][,loan_to_income := loan_amount/income
-                                               # Binning variables into deciles
-                                             ][,dec_loan_amount := cut(loan_amount,
-                                                                       quantile(loan_amount,probs=seq(1,0,by=-0.1)),
-                                                                       include.lowest=T,labels=F)
-                                               ][,loan_to_value_ratio := loan_to_value_ratio + rnorm(length(loan_to_value_ratio))*1e-10
-                                               ][,dec_loan_to_value := cut(loan_to_value_ratio,
-                                                                           quantile(loan_to_value_ratio,probs=seq(1,0,by=-0.1)),
+                                                 ][,loan_to_income := loan_amount/income
+                                                   # Binning variables into deciles
+                                                 ][,dec_loan_amount := cut(loan_amount,
+                                                                           quantile(loan_amount,probs=seq(1,0,by=-0.1)),
                                                                            include.lowest=T,labels=F)
-                                               ][,dec_income := cut(income, 
-                                                                    quantile(income,probs=seq(1,0,by=-0.1)),
-                                                                    include.lowest=T,labels=F)
-                                               ][,dec_property_value := cut(property_value,
-                                                                            quantile(property_value,probs=seq(1,0,by=-0.1)),
-                                                                            include.lowest=T,labels=F)
-                                               ][,dec_loan_to_income := cut(loan_to_income, 
-                                                                            quantile(loan_to_income,probs=seq(1,0,by=-0.1)),
-                                                                            include.lowest=T,labels=F)]
+                                                   ][,loan_to_value_ratio := loan_to_value_ratio + rnorm(length(loan_to_value_ratio))*1e-10
+                                                   ][,dec_loan_to_value := cut(loan_to_value_ratio,
+                                                                               quantile(loan_to_value_ratio,probs=seq(1,0,by=-0.1)),
+                                                                               include.lowest=T,labels=F)
+                                                   ][,dec_income := cut(income, 
+                                                                        quantile(income,probs=seq(1,0,by=-0.1)),
+                                                                        include.lowest=T,labels=F)
+                                                   ][,dec_property_value := cut(property_value,
+                                                                                quantile(property_value,probs=seq(1,0,by=-0.1)),
+                                                                                include.lowest=T,labels=F)
+                                                   ][,dec_loan_to_income := cut(loan_to_income, 
+                                                                                quantile(loan_to_income,probs=seq(1,0,by=-0.1)),
+                                                                                include.lowest=T,labels=F)]
 }
 
+gc()
 hmda2018_cl <- new_list[[1]]
 hmda2019_cl <- new_list[[2]]
 hmda2020_cl <- new_list[[3]]
@@ -76,7 +86,7 @@ hmda_cl <- rbind(hmda2018_cl,hmda2019_cl,hmda2020_cl,hmda2021_cl)
 rm(raw_list,hmda2018,hmda2019,hmda2020,hmda2021,hmda2018_cl,hmda2019_cl,hmda2020_cl,hmda2021_cl)
 hmda_cl[,year_fips := paste0(year,"-",st_cnty_fips)]
 setkey(hmda_cl, year_fips)
-
+gc()
 ## Wrangling HMDA data
 hmda_match <- hmda_cl[,.(year,lei,census_tract,year_fips,rate_spread,us30_spread,
                          applicant_race1,applicant_age,
@@ -90,9 +100,11 @@ hmda_match <- hmda_cl[,.(year,lei,census_tract,year_fips,rate_spread,us30_spread
                          over_conflimit)]
 
 ## Relabel data
-hmda_match[,race := ifelse(applicant_race1 == "White",1,2)]
-hmda_match[applicant_race1 == "Joint", race := 3]
-hmda_match[applicant_race1 == "Free Form Text Only", race := NA]
+hmda_match[,race := 4]
+hmda_match[applicant_race1 == 5, race := 1]
+hmda_match[applicant_race1 %in% c(2,21,22,23,24,25,26,27), race := 2]
+hmda_match[applicant_race1 == 3, race := 3]
+hmda_match[is.na(applicant_race1),race := NA]
 hmda_match[,applicant_race1 := NULL]
 
 # hmda_match[derived_sex == "Female", derived_sex := 1]
@@ -112,7 +124,6 @@ hmda_match[applicant_age == "9999", applicant_age := NA]
 
 ## Change data into factor variables
 hmda_match[,race := as.factor(race)]
-# hmda_match[,derived_sex := as.factor(derived_sex)]
 hmda_match[,applicant_age := as.factor(applicant_age)]
 hmda_match[,purpose := as.factor(purpose)]
 hmda_match[,over_conflimit := as.factor(over_conflimit)]
@@ -120,3 +131,4 @@ hmda_match[,over_conflimit := as.factor(over_conflimit)]
 # Export HMDA sample
 saveRDS(hmda_match,file = paste0(wd,hmda.folder,"hmda_match.rds"))
 saveRDS(hmda_cl,file = paste0(wd,hmda.folder,"hmda_clean.rds"))
+
